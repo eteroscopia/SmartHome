d997f283f0304435645913267eba14ce8a33a580
Ernesto Salazar <Neto93@macbook-pro-de-ernesto.local>
2017-03-30 17:41:52 -0700

    Initial Commit

---
+++ SmartHomePedro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartHomePedro
+//
+//  Created by Ernesto Salazar on 30/03/17.
+//  Copyright © 2017 DreamTeamCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
